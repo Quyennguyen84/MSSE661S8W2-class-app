105d32353ac6b797afadab22899000281a1f42fb
Quyennguyen84 <125732408+Quyennguyen84@users.noreply.github.com>
2023-03-20 00:29:51 -0600

    Add files via upload

---
+++ week 2/src/app.js
@@ -0,0 +1,15 @@
+var object = { 
+    id: "piano",
+    price: "500"
+};
+class  Thing {
+    id;
+    price;
+    constructor(id,price)
+    this.id = id
+    this.price = price
+}
+getLegal()
+{
+    return this.id < 300
+}
